88bbc6b4ac9be802518d8bbde0183229d7f75014
Marketing-Digital8 <62025852+Marketing-Digital8@users.noreply.github.com>
2020-03-30 12:46:42 +0300

    Update codes.js

--- js/codes.js
+++ js/codes.js
@@ -1,18 +1,13 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-7139XLLZDY"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+/* Global site tag (gtag.js) - Google Analytics */
+  
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-    gtag('config', 'G-7139XLLZDY');
-  </script>
+gtag('config', 'G-7139XLLZDY');
 
-  <!--   search console verify   -->
-  <meta name="google-site-verification" content="RunM4nI6xULuXmvwz2dGM_mrs_9Dw_zcPsY9CwGvduI" />
-
-<!-- Facebook Pixel Code -->
-<script>
+/* Facebook Pixel Code */
+  
   !function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -23,8 +18,8 @@
   'https://connect.facebook.net/en_US/fbevents.js');
   fbq('init', '2767238990053777');
   fbq('track', 'PageView');
-</script>
+
 <noscript><img height="1" width="1" style="display:none"
   src="https://www.facebook.com/tr?id=2767238990053777&ev=PageView&noscript=1"
 /></noscript>
-<!-- End Facebook Pixel Code -->
+/* End Facebook Pixel Code */
